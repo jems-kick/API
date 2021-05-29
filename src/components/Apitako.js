35dcc6c3dd626793570ded933590bded9094a74b
jems-kick <kakadiyajemish797@gmail.com>
2021-05-29 10:03:48 +0530

    13

--- src/components/Apitako.js
+++ src/components/Apitako.js
@@ -79,37 +79,46 @@ class API extends React.Component {
         }
 
         const style = {
-            margin: "100px"
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center center',
+            marginTop: '11.5%',
+            marginLeft: '35%'
+        }
+        const style1 = {
+            marginTop: '0%',
+            marginLeft: '10%'
         }
 
         return (
             <div>
-                <div className="Center">
-                    <strong>
-                        <input type="text" placeholder="Enetr URL" onChange={this.onUrlChnagehendler}></input>
-                    </strong>
-                    <div className="btn">
-                        <button className="button" onClick={this.AppEnginehendler}>Submi</button>
-                    </div>
-                    <div>
-                        {
-                            !this.state.isDone
-                                ? (
-                                    <div style={style}>
-                                        <ReactLoading
-                                            type={'balls'}
-                                            color={'#009933'}
-                                            height={50}
-                                        />
-                                    </div>
-                                ) : (
-                                    <div className="users">
-                                        {ForData}
-                                    </div>
-                                )
-                        }
-                    </div>
-                </div>
+                {
+                    !this.state.isDone
+                        ? (
+                            <div style={style}>
+                                <strong style={{ color: "white", fontSize: "200%" }}>Loading your data...</strong>
+                                <div style={style1}>
+                                    <ReactLoading
+                                        type={'cylon'}
+                                        color={'#009933'}
+                                        height={166} width={93}
+                                    />
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="Center">
+                                <strong>
+                                    <input type="text" placeholder="Enetr URL" onChange={this.onUrlChnagehendler}></input>
+                                </strong>
+                                <div className="btn">
+                                    <button className="button" onClick={this.AppEnginehendler}>Submit</button>
+                                </div>
+                                <div className="users">
+                                    {ForData}
+                                </div>
+                            </div>
+                        )}
             </div >
         )
     }
